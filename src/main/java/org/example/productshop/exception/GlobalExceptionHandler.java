e94652c300c61653519a86899af6d0c73cda4748
ca46558 <a0909224635@gmail.com>
2025-08-08 00:44:38 +0800

    新增ExceptionHandler(RuntimeException)

--- src/main/java/org/example/productshop/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/productshop/exception/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -29,5 +32,31 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleOtherErrors(Exception e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("server error");
+    }
+
+    //-----加入購物車------
+    @ExceptionHandler(OutOfStockException.class)
+    public ResponseEntity<?> handleOutOfStock(OutOfStockException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "OUT_OF_STOCK");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.badRequest().body(response);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<?> handleProductNotFound(ProductNotFoundException ex) {
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "NOT_FOUND");
+        response.put("message", ex.getMessage());
+        return ResponseEntity.status(404).body(response);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<?> handleRuntime(RuntimeException ex) {
+        ex.printStackTrace(); // 或log.error("Exception: ", ex)
+        Map<String, String> response = new HashMap<>();
+        response.put("error", "RUNTIME_ERROR");
+        response.put("message", "執行期間發生錯誤");
+        return ResponseEntity.status(500).body(response);
     }
 }
